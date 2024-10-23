c96e564f883ded5e9b6ab5c732109d9b41a495ce
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-24 04:05:50 +0600

    create user with email and password

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -1,8 +1,13 @@
 import { createContext, useEffect, useState } from "react";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {
+    createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+} from "firebase/auth";
 import { app } from "../firebase/firebase.config";
 import PropTypes from "prop-types";
-
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
@@ -10,26 +15,42 @@ const auth = getAuth(app);
 const Context = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
-  useEffect(()=>{
-    const unsubscribe= onAuthStateChanged(auth,(currentUser=>{
-        setUser(currentUser);
-        setLoading(false);
-    }))
-    return ()=>{
-        return unsubscribe()
-    }
-  },[])
+  const googleProvider = new GoogleAuthProvider();
+
+  //   Sign In with Email & Password
+  const createUser=(email,password)=>{
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth,email,password);
+  }
+
+  // Sign In with Google
+  const signInWithGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
   const authInfo = {
     user,
     loading,
+    signInWithGoogle,
+    createUser,
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
 };
 
-Context.propTypes={
-    children:PropTypes.node.isRequired,
-}
+Context.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default Context;
