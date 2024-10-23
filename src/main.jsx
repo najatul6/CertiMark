6d3e267e3d6426b0f3942e1d83dcd0b1770e48a0
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-24 04:38:44 +0600

    add react helmet async

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import Routes from "./Router/Routes.jsx";
 import "./index.css";
 import AuthProvider from "./AuthProvider/AuthProvider.jsx";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HelmetProvider>
     <AuthProvider>
-    <Toaster />
-      <RouterProvider router={Routes} />
+        <Toaster />
+        <RouterProvider router={Routes} />
     </AuthProvider>
-  </StrictMode>
+  </HelmetProvider>
+
 );
