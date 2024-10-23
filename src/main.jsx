bd8a14543b4b058cf49af334a8d8a9108df2be33
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-24 04:40:43 +0600

    create new hook for user Auth

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,13 @@ import Routes from "./Router/Routes.jsx";
 import "./index.css";
 import AuthProvider from "./AuthProvider/AuthProvider.jsx";
 import { Toaster } from "react-hot-toast";
-
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <HelmetProvider>
     <AuthProvider>
-        <Toaster />
-        <RouterProvider router={Routes} />
+      <Toaster />
+      <RouterProvider router={Routes} />
     </AuthProvider>
   </HelmetProvider>
-
 );
--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,8 +1,8 @@
 import { FcGoogle } from 'react-icons/fc';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import useAuth from '../../hooks/useAuth';
 import toast from 'react-hot-toast';
 import { TbFidgetSpinner } from 'react-icons/tb';
+import useAuth from '../../hooks/useAuth';
 
 const LogIn = () => {
   const { createUser, signInWithGoogle, loading } = useAuth();
