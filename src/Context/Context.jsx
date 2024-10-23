c5a9c6ef663388f2145c74e0548435b523d78235
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-24 04:01:19 +0600

    update context and set user and prop validate

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { app } from "../firebase/firebase.config";
+import PropTypes from "prop-types";
 
-const { createContext } = require("react");
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
@@ -8,6 +10,15 @@ const auth = getAuth(app);
 const Context = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  useEffect(()=>{
+    const unsubscribe= onAuthStateChanged(auth,(currentUser=>{
+        setUser(currentUser);
+        setLoading(false);
+    }))
+    return ()=>{
+        return unsubscribe()
+    }
+  },[])
   const authInfo = {
     user,
     loading,
@@ -16,5 +27,9 @@ const Context = ({ children }) => {
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
 };
+
+Context.propTypes={
+    children:PropTypes.node.isRequired,
+}
 
 export default Context;
