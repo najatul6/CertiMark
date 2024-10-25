8176fce1ee919f968eaba498925c3e59d7ae1ad6
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-25 17:03:28 +0600

    update log in page

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -46,7 +46,7 @@ const LogIn = () => {
   };
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900">
+      <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-white/10  text-white">
         <div className="mb-8 text-center">
           <h1 className="my-3 text-4xl font-bold">Log In</h1>
           <p className="text-sm text-gray-400">
@@ -70,7 +70,7 @@ const LogIn = () => {
                 id="email"
                 required
                 placeholder="Enter Your Email Here"
-                className="w-full px-3 py-2 rounded-md  focus:outline-none bg-gray-200 text-gray-900"
+                className="w-full px-3 py-2 rounded-md  focus:outline-none bg-transparent border border-lightTeal text-gray-900"
                 data-temp-mail-org="0"
               />
             </div>
@@ -87,7 +87,7 @@ const LogIn = () => {
                 id="password"
                 required
                 placeholder="*******"
-                className="w-full px-3 py-2 border rounded-md  focus:outline-none bg-gray-200 text-gray-900"
+                className="w-full px-3 py-2  rounded-md  focus:outline-none bg-transparent border-lightTeal border text-white "
               />
             </div>
           </div>
@@ -106,7 +106,7 @@ const LogIn = () => {
           </div>
         </form>
         <Link to="/forgetPassword" className="space-y-1 flex justify-end py-2">
-          <button className="text-sm hover:font-bold hover:underline hover:text-teal text-gray-400">
+          <button className="text-sm hover:font-bold hover:underline hover:text-teal text-gray-500">
             Forgot password?
           </button>
         </Link>
@@ -129,7 +129,7 @@ const LogIn = () => {
           Don&apos;t have an account yet?{" "}
           <Link
             to="/register"
-            className="hover:underline hover:text-deep-orange text-gray-600"
+            className="hover:underline hover:text-deep-orange text-gray-500"
           >
             Sign up
           </Link>
