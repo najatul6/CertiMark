c738a6eeae323c5d4f901648585bf169090d27ba
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-31 18:23:40 +0600

    add some data in download section

--- src/pages/Dashboard/UserDashboard/Downloads/Downloads.jsx
+++ src/pages/Dashboard/UserDashboard/Downloads/Downloads.jsx
@@ -2,7 +2,7 @@
 const Downloads = () => {
   return (
     <div>
-      Downloads
+      <img src="../../../../assets/Screenshot 2024-10-15 204703.png" alt="Transcript Certificate" />
     </div>
   )
 }
