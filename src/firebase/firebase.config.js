082bc61a49fa54a427d1dc9641307ec2d5a5bc39
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-31 18:19:52 +0600

    update firebase config for authentication

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY_FIREBASE,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN_FIREBASE,
+  apiKey: import.meta.env.VITE_API_FIREBASE,
+  authDomain: import.meta.env.VITE_DOMAIN_FIREBASE,
   projectId: import.meta.env.VITE_PROJECTID_FIREBASE,
   storageBucket: import.meta.env.VITE_STORAGEBUCKET_FIREBASE,
   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID_FIREBASE,
-  appId: import.meta.env.VITE_APPID_FIREBASE,
+  appId: import.meta.env.VITE_APP_FIREBASE,
 };
 
 export const app = initializeApp(firebaseConfig);
