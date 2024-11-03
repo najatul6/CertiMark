0b907cc83a58269ca90f1f32dc58f8396b976d1b
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-04 04:24:33 +0600

    create new hook for application

--- src/hooks/useApplication.jsx
+++ src/hooks/useApplication.jsx
@@ -1,8 +1,16 @@
+import { useQuery } from "@tanstack/react-query"
+import useAxiosSecure from "./useAxiosSecure"
 
-const useApplication = () => {
-  return (
-    <div>useApplication</div>
-  )
+const useApplication =() => { 
+    const axiosSecure=useAxiosSecure()
+    const {data:application=[]}=useQuery({
+        queryKey:["application"],
+        queryFn:async()=>{
+            const res = await axiosSecure.get('/applicants')
+            return res.data
+        }
+    })
+  return [application]
 }
 
 export default useApplication
