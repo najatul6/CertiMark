0562c1162e43b98fad472252112410233e2a59f7
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-04 04:44:33 +0600

    update to get only user data

--- src/hooks/useApplication.jsx
+++ src/hooks/useApplication.jsx
@@ -1,16 +1,18 @@
 import { useQuery } from "@tanstack/react-query"
 import useAxiosSecure from "./useAxiosSecure"
+import useAuth from "./useAuth"
 
 const useApplication =() => { 
     const axiosSecure=useAxiosSecure()
-    const {data:application=[]}=useQuery({
-        queryKey:["application"],
+    const {user}=useAuth()
+    const {data:application=[],refetch}=useQuery({
+        queryKey:["application",user?.email],
         queryFn:async()=>{
-            const res = await axiosSecure.get('/applicants')
+            const res = await axiosSecure.get(`/applicants?email=${user?.email}`)
             return res.data
         }
     })
-  return [application]
+  return [application,refetch]
 }
 
 export default useApplication
--- src/pages/Dashboard/UserDashboard/UserOverview/UserOverview.jsx
+++ src/pages/Dashboard/UserDashboard/UserOverview/UserOverview.jsx
@@ -1,21 +1,24 @@
 import { useEffect, useState } from "react";
 import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 import toast from "react-hot-toast";
+import useApplication from "../../../../hooks/useApplication";
 
 const UserOverview = () => {
   const [pendingApplication, setPendingApplication] = useState([]);
   const axiosSecure = useAxiosSecure();
+  const [,refetch]=useApplication()
   useEffect(() => {
     axiosSecure
       .get("/applicants")
       .then((response) => {
-        setPendingApplication(response.data);
+        setPendingApplication(response.data)
+        refetch()
       })
       .catch((error) => {
         toast.error("Failed to fetch pending applications.");
         console.error(error);
       });
-  }, [axiosSecure]);
+  }, [axiosSecure,refetch]);
   return (
     <div className="">
       <h2 className="text-2xl font-bold mb-6">My Dashboard Overview</h2>
