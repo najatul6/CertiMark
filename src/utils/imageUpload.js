c889874d9c29676760f75e97621978cebc9dd30b
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-03 20:57:51 +0600

    Update image upload api

--- src/utils/imageUpload.js
+++ src/utils/imageUpload.js
@@ -4,7 +4,7 @@ export const imageUpload = async (image) => {
     const formData = new FormData();
     formData.append("image", image);
     const { data } = await axios.post(
-      `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_KEY}`,
+      `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_APP}`,
       formData
     );
     return data
