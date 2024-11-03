182bad10813dbdcb562ca0fe5dde9017ea8a806f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-04 04:15:59 +0600

    add query client

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,16 @@ import "./index.css";
 import AuthProvider from "./AuthProvider/AuthProvider.jsx";
 import { Toaster } from "react-hot-toast";
 import { HelmetProvider } from "react-helmet-async";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// Create a client
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <HelmetProvider>
-    <AuthProvider>
-      <Toaster />
-      <RouterProvider router={Routes} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Toaster />
+        <RouterProvider router={Routes} />
+      </AuthProvider>
+    </QueryClientProvider>
   </HelmetProvider>
 );
