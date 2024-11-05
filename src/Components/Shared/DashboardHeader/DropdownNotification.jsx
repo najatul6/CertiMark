80c44f3a6fd6acc0e689b6ae9e119ba6944ae560
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-05 23:58:49 +0600

    update notification button

--- src/Components/Shared/DashboardHeader/DropdownNotification.jsx
+++ src/Components/Shared/DashboardHeader/DropdownNotification.jsx
@@ -1,31 +1,41 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const DropdownNotification = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [notifying, setNotifying] = useState(true);
 
   return (
-    <div>
-        <button onClick={()=>setDropdownOpen(dropdownOpen)}>{dropdownOpen}</button>
+    <div className="relative">
+      <button onClick={() => setDropdownOpen(!dropdownOpen)}>
+        {dropdownOpen ? "close" : "open"}
+      </button>
+      {dropdownOpen && (
+        <div className="absolute right-0 mt-2.5 w-80 flex flex-col  rounded-xl border-stroke bg-darkGreen shadow-default border">
+          <div className="px-4.5 py-3">
+            <h5 className="text-sm font-medium text-red">Notification</h5>
+          </div>
+
+          <ul className="flex flex-col overflow-y-auto mt-5">
+            <li>
+              <Link
+                className="flex flex-col gap-2.5 border-t border-stroke px-4.5 py-3 hover:bg-red-500"
+                to="#"
+              >
+                <p className="text-sm">
+                  <span className="text-black ">
+                    Edit your information in a swipe
+                  </span>{" "}
+                  ...
+                </p>
+                <p className="text-xs">12 May, 2025</p>
+              </Link>
+            </li>
+            {/* Repeat similar items for notifications */}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
 
 export default DropdownNotification;
-
-
-{/* <div className="absolute right-0 mt-2.5 w-80 flex flex-col rounded-sm  border-stroke bg-red-600 shadow-default border">
-<div className="px-4.5 py-3">
-  <h5 className="text-sm font-medium text-red">Notification</h5>
-</div>
-
-<ul className="flex flex-col overflow-y-auto">
-  <li>
-    <Link className="flex flex-col gap-2.5 border-t border-stroke px-4.5 py-3 hover:bg-red-500" to="#">
-      <p className="text-sm"><span className="text-black ">Edit your information in a swipe</span> ...</p>
-      <p className="text-xs">12 May, 2025</p>
-    </Link>
-  </li>
-  {/* Repeat similar items for notifications */}
-// </ul>
-// </div> */}
