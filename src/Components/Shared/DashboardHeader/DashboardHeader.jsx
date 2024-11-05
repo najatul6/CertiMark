0ad1bab260a7808665b27e009c3e46c699ca52b4
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-06 00:14:47 +0600

    update sidebar

--- src/Components/Shared/DashboardHeader/DashboardHeader.jsx
+++ src/Components/Shared/DashboardHeader/DashboardHeader.jsx
@@ -3,14 +3,14 @@ import DropdownNotification from "./DropdownNotification";
 
 const DashboardHeader = ({ sidebarOpen, setSidebarOpen }) => {
   return (
-    <header className="sticky top-0 z-999 flex w-full bg-teal drop-shadow-1">
+    <header className="sticky top-0 z-999 flex w-full bg-lightTeal drop-shadow-xl">
       <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
         
         <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
           <button
             aria-controls="sidebar"
             onClick={() => setSidebarOpen(!sidebarOpen)}
-            className="z-99999 block rounded-sm border border-stroke bg-red-600 p-1.5 shadow-sm dark:border-strokedark dark:bg-boxdark lg:hidden"
+            className="z-99999 block rounded-sm border border-stroke bg-red-600 p-1.5 shadow-sm lg:hidden"
           >
             {/* Hamburger Icon */}
             <span className="relative block h-5.5 w-5.5 cursor-pointer">
@@ -24,25 +24,7 @@ const DashboardHeader = ({ sidebarOpen, setSidebarOpen }) => {
             <span className="text-2xl font-bold">Certi<span className="text-lightTeal">Mark</span>.</span>
           </Link>
         </div>
-
-        {/* Search Bar */}
-        <div className="hidden sm:block">
-          <form action="#" method="POST">
-            <div className="relative">
-              <button className="absolute left-0 top-1/2 -translate-y-1/2">
-                <svg className="fill-body dark:fill-bodydark" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path fillRule="evenodd" clipRule="evenodd" d="..." fill="" />
-                </svg>
-              </button>
-              <input
-                type="text"
-                placeholder="Type to search..."
-                className="w-full bg-transparent pl-9 pr-4 text-black focus:outline-none dark:text-white xl:w-125"
-              />
-            </div>
-          </form>
-        </div>
-
+        
         <div className="flex items-center gap-3 2xsm:gap-7">
           <ul className="flex items-center gap-2 2xsm:gap-4">
             <DropdownNotification />
