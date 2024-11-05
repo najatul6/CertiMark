d77028132480bdf4f9de17a83c765ad26c615520
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-05 18:20:44 +0600

    update application status from

--- src/pages/Dashboard/UserDashboard/ApplicationStatus/ApplicationStatus.jsx
+++ src/pages/Dashboard/UserDashboard/ApplicationStatus/ApplicationStatus.jsx
@@ -1,28 +1,31 @@
+import useApplication from "../../../../hooks/useApplication";
+
 const ApplicationStatus = () => {
+  const [application] = useApplication();
   return (
-    <div className="min-h-screen bg-[#FEFFFF] p-4 md:p-6 flex flex-col items-center">
+    <div className="min-h-screen bg-[#FEFFFF] md:p-6 flex flex-col items-center">
       <h2 className="text-2xl font-semibold text-darkGreen mb-4 text-center md:text-left">
         Application Status
       </h2>
-      {/* <div className="bg-white p-4 md:p-6 shadow-lg rounded-lg w-full max-w-3xl">
+      <div className="bg-white  md:p-6 shadow-lg rounded-lg w-full max-w-3xl">
         <div className="overflow-x-auto">
           <table className="w-full text-left border-collapse">
             <thead>
               <tr>
-                <th className="border-b py-2 px-4 whitespace-nowrap">
+                <th className="border-b py-2 px-2 whitespace-nowrap text-wrap">
                   Application Type
                 </th>
-                <th className="border-b py-2 px-4 whitespace-nowrap">
+                <th className="border-b py-2 px-2 whitespace-nowrap text-wrap">
                   Submission Date
                 </th>
-                <th className="border-b py-2 px-4 whitespace-nowrap">Status</th>
+                <th className="border-b py-2 px-2 whitespace-nowrap text-wrap">Status</th>
               </tr>
             </thead>
             <tbody>
-              {applications.map((app, index) => (
+              {application.map((app, index) => (
                   <tr key={index} className="hover:bg-gray-100">
-                    <td className="border-b py-2 px-4">{app.type}</td>
-                    <td className="border-b py-2 px-4">{app.submissionDate}</td>
+                    <td className="border-b py-2 px-4">{app?.certificateType}</td>
+                    <td className="border-b py-2 px-4">{app?.submissionDate}</td>
                     <td className="border-b py-2 px-4">
                       <span
                         className={`px-2 py-1 rounded-full text-sm ${
@@ -41,7 +44,7 @@ const ApplicationStatus = () => {
             </tbody>
           </table>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };
