01e2ba83b43268bd12f4f799a0644c07e836fe12
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-05 18:14:40 +0600

    update user dashboard

--- src/pages/Dashboard/UserDashboard/UserOverview/UserOverview.jsx
+++ src/pages/Dashboard/UserDashboard/UserOverview/UserOverview.jsx
@@ -35,60 +35,55 @@ const UserOverview = () => {
 
   return (
     <div className="">
-      <h2 className="text-2xl font-bold mb-6">My Dashboard Overview</h2>
+      <h2 className="text-2xl font-bold mb-6 text-center py-5 text-darkGreen">My Dashboard Overview</h2>
 
       {/* User-specific overview cards */}
-      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-6">
         {/* Pending Applications */}
-        <div className="bg-white shadow rounded-lg p-4">
+        <div className="bg-lightTeal shadow rounded-lg p-10">
           <h3
-            className="font-semibold"
-            style={{ fontFamily: "Roboto", fontSize: "16px" }}
+            className="font-semibold text-darkGreen text-xl pb-5 "
           >
             My Pending Applications
           </h3>
-          <p className="text-gray-600">{application?.length}</p>
+          <p className="text-white text-lg pl-4">{application?.length}</p>
         </div>
 
         {/* Verified Certificates */}
-        <div className="bg-white shadow rounded-lg p-4">
+        <div className="bg-lightTeal shadow rounded-lg p-10">
           <h3
-            className="font-semibold"
-            style={{ fontFamily: "Roboto", fontSize: "16px" }}
+            className="font-semibold text-darkGreen text-xl pb-5 "
           >
             Verified Certificates
           </h3>
-          <p className="text-gray-600">{verifiedApplication?.length}</p>
+          <p className="text-white text-lg pl-4">{verifiedApplication?.length}</p>
         </div>
 
         {/* Total Paid */}
-        <div className="bg-white shadow rounded-lg p-4">
+        <div className="bg-lightTeal shadow rounded-lg p-10">
           <h3
-            className="font-semibold"
-            style={{ fontFamily: "Roboto", fontSize: "16px" }}
+            className="font-semibold text-darkGreen text-xl pb-5 "
           >
             Total Paid
           </h3>
-          <p className="text-gray-600">&#x9F3; {totalPaid}</p>
+          <p className="text-white text-lg pl-4">&#x9F3; {totalPaid}</p>
         </div>
 
         {/* Total Due */}
-        <div className="bg-white shadow rounded-lg p-4">
+        <div className="bg-lightTeal shadow rounded-lg p-10">
           <h3
-            className="font-semibold"
-            style={{ fontFamily: "Roboto", fontSize: "16px" }}
+            className="font-semibold text-darkGreen text-xl pb-5 "
           >
             Total Due
           </h3>
-          <p className="text-gray-600">&#x9F3; {totalUnPaid}</p>
+          <p className="text-white text-lg pl-4">&#x9F3; {totalUnPaid}</p>
         </div>
       </section>
-
-      {/* Recent Activity */}
+      {/* Recent Activity
       <section className="mt-8">
         <h3 className="font-semibold mb-4 text-lg">Recent Activity</h3>
 
-        {/* <div className="bg-white shadow rounded-lg border border-red-600">
+        <div className="bg-white shadow rounded-lg border border-red-600">
           <div className="overflow-x-auto">
             <table className="min-w-full table-auto">
               <thead className="bg-lightTeal text-white uppercase">
@@ -133,8 +128,8 @@ const UserOverview = () => {
               </tbody>
             </table>
           </div>
-        </div> */}
-      </section>
+        </div>
+      </section> */}
     </div>
   );
 };
