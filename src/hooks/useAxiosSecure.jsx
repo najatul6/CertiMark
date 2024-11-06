0b47a001756b9873351dbb1c843c4043e0dfb671
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-07 03:32:49 +0600

    If session expired, log out and redirect to login page

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,9 +1,13 @@
 import axios from "axios";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
 });
 const useAxiosSecure = () => {
+  const navigate=useNavigate()
+  const {logOut}=useAuth()
   // Add request interceptor to attach token to each request
   axiosSecure.interceptors.request.use(function (config) {
     const token = localStorage.getItem("access-token");
@@ -13,13 +17,18 @@ const useAxiosSecure = () => {
     function (err) {
       return Promise.reject(err);
     };
-    // Add response interceptor to handle session expiration
+  // Add response interceptor to handle session expiration
   axiosSecure.interceptors.response.use(
     function (response) {
       return response;
     },
-    (error) => {
-      const status=error.response?.status
+    async(error) => {
+      const status = error.response?.status;
+      // If session expired, log out and redirect to login page
+      if(status === 401 || status ===403){
+        await logOut();
+        navigate("/login");
+      }
       console.log(status);
       return Promise.reject(error);
     }
