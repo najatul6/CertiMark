0f729e3e582b3b90090abdff79639a998df76e8b
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-08 01:14:55 +0600

    remove unused lines

--- src/hooks/useRole.jsx
+++ src/hooks/useRole.jsx
@@ -9,7 +9,6 @@ const useRole = () => {
     queryKey: [user?.email, "userRole"],
     queryFn: async () => {
       const res = await axiosSecure.get(`/users/admin/${user?.email}`);
-      // console.log(res.data);
       return res.data?.admin;
     },
   });
