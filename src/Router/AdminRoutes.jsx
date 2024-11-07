01258aebe8b43692cfc496d39b507fc742a6f51e
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-07 23:55:40 +0600

    update admin route

--- src/Router/AdminRoutes.jsx
+++ src/Router/AdminRoutes.jsx
@@ -1,12 +1,21 @@
+import { Navigate, useLocation } from "react-router"
 import useAuth from "../hooks/useAuth"
 import useRole from "../hooks/useRole"
+import Loading from "../Components/Shared/Loading/Loading"
 
-const AdminRoutes = () => {
-    const {user,isLoading  }=useAuth()
+const AdminRoutes = ({children}) => {
+    const {user,loading  }=useAuth()
     const [userRole,isAdminLoading]=useRole()
-  return (
-    <div>AdminRoutes</div>
-  )
+    const location=useLocation()
+    if(loading){
+        return <Loading/>
+    }
+    if(user){
+        return children
+    }
+
+    return <Navigate to="/logIn" state={{from:location}}/>
+  
 }
 
 export default AdminRoutes
