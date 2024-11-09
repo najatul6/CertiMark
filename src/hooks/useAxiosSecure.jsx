080f1786ee549b2a8c35644d1457bad265a3a3da
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-10 05:22:04 +0600

    update axios secure

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -6,35 +6,40 @@ import toast from "react-hot-toast";
 const axiosSecure = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
 });
+
 const useAxiosSecure = () => {
-  const navigate=useNavigate()
-  const {logOut}=useAuth()
+  const navigate = useNavigate();
+  const { logOut } = useAuth();
+
   // Add request interceptor to attach token to each request
-  axiosSecure.interceptors.request.use(function (config) {
-    const token = localStorage.getItem("access-token");
-    config.headers.authorization = `Bearer ${token}`;
-    return config;
-  }),
-    function (err) {
-      return Promise.reject(err);
-    };
-  // Add response interceptor to handle session expiration
-  axiosSecure.interceptors.response.use(
-    function (response) {
-      return response;
-    },
-    async(error) => {
-      const status = error.response?.status;
-      // If session expired, log out and redirect to login page
-      if(status === 401 || status ===403){
-        await logOut();
-        toast.error('Session expired, please log in again');
-        navigate("/login");
+  axiosSecure.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem("access-token");
+      if (token) {
+        config.headers.authorization = `Bearer ${token}`;
       }
-      console.log(status);
+      return config;
+    },
+    (error) => {
       return Promise.reject(error);
     }
   );
+
+  // Add response interceptor to handle session expiration
+  axiosSecure.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      const status = error.response?.status;
+      // If session expired, log out and redirect to login page
+      if (status === 401 || status === 403) {
+        await logOut();
+        toast.error("Session expired, please log in again");
+        navigate("/login");
+      }
+      return Promise.reject(error);
+    }
+  );
+
   return axiosSecure;
 };
 
