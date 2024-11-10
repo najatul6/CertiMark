34925f3bb15d0ce324a9eba7192c12688f1d1cde
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-11 03:04:50 +0600

    update reject application page

--- src/pages/Dashboard/AdminDashboard/RejectedApplications/RejectedApplications.jsx
+++ src/pages/Dashboard/AdminDashboard/RejectedApplications/RejectedApplications.jsx
@@ -5,7 +5,7 @@ import Swal from "sweetalert2";
 import useApplicants from "../../../../hooks/useApplicants";
 
 const RejectedApplications = () => {
-  const [applicants,refetch,isPending]=useApplicants()
+  const [applicants, refetch, isPending] = useApplicants();
   const axiosSecure = useAxiosSecure();
   // Function to format date to AM/PM
   const formatDate = (dateString) => {
@@ -22,9 +22,7 @@ const RejectedApplications = () => {
   };
 
   // Filter Applications by Status
-  const filterSearch = applicants.filter(
-    (user) => user.Status === "Rejected"
-  );
+  const filterSearch = applicants.filter((user) => user.Status === "Rejected");
 
   // Delete Application from Database
   const handleDeleteApplication = (application) => {
@@ -40,7 +38,7 @@ const RejectedApplications = () => {
     }).then((result) => {
       if (result.isConfirmed) {
         axiosSecure.delete(`/applications/${application?._id}`).then((res) => {
-            console.log(res.data);
+          console.log(res.data);
           if (res.data.deletedCount > 0) {
             refetch();
             Swal.fire({
@@ -50,7 +48,7 @@ const RejectedApplications = () => {
             });
           }
         });
-      }else {
+      } else {
         Swal.fire({
           title: "Failed!",
           text: "Deletion unsuccessful.",
@@ -112,65 +110,82 @@ const RejectedApplications = () => {
                 </tr>
               </thead>
               <tbody className="whitespace-nowrap">
-                {filterSearch.map((application) => {
-                  return (
-                    <tr
-                      key={application?._id}
-                      className="border-b text-lightTeal text-wrap "
+                {filterSearch.length === 0 ? (
+                  // No Data Found Row
+                  <tr>
+                    <td
+                      colSpan="9"
+                      className="py-10 text-center text-2xl font-bold text-white"
                     >
-                      <td className="p-2 text-sm border-r">
-                        {application?.name}
-                      </td>
-                      <td className="p-2 text-sm border-r">
-                        {application?.studentId}
-                      </td>
-                      <td className="p-2 text-sm border-r">
-                        {application?.registrationNo}
-                      </td>
-                      <td className="p-2 text-sm border-r">
-                        {application?.technology}
-                      </td>
-                      <td className="p-2 text-sm border-r">
-                        {application?.certificateType}
-                      </td>
-                      <td className="p-2 text-sm border-r">
-                        {application?.yearOfCompletion}
-                      </td>
-                      <td className="p-2 text-sm border-r">
-                        {formatDate(application?.ApplyDate)}
-                      </td>
-                      <td
-                        className={`p-2 text-sm border-r text-center uppercase ${
-                          application?.fee === "unPaid" && "text-red-600"
-                        }`}
-                      >
-                        {application?.fee}
-                      </td>
-                      <td className="py-2 px-1 flex flex-col justify-center items-center gap-2 text-sm capitalize">
-                      <button
-                          className="mr-4 btn"
-                          title="Delete"
-                          onClick={() => handleDeleteApplication(application)}
+                      No Data Found...
+                    </td>
+                  </tr>
+                ) : (
+                  // Data Rows
+                  <>
+                    {filterSearch.map((application) => {
+                      return (
+                        <tr
+                          key={application?._id}
+                          className="border-b text-lightTeal text-wrap "
                         >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-5 fill-red-500 hover:fill-red-700"
-                            viewBox="0 0 24 24"
+                          <td className="p-2 text-sm border-r">
+                            {application?.name}
+                          </td>
+                          <td className="p-2 text-sm border-r">
+                            {application?.studentId}
+                          </td>
+                          <td className="p-2 text-sm border-r">
+                            {application?.registrationNo}
+                          </td>
+                          <td className="p-2 text-sm border-r">
+                            {application?.technology}
+                          </td>
+                          <td className="p-2 text-sm border-r">
+                            {application?.certificateType}
+                          </td>
+                          <td className="p-2 text-sm border-r">
+                            {application?.yearOfCompletion}
+                          </td>
+                          <td className="p-2 text-sm border-r">
+                            {formatDate(application?.ApplyDate)}
+                          </td>
+                          <td
+                            className={`p-2 text-sm border-r text-center uppercase ${
+                              application?.fee === "unPaid" && "text-red-600"
+                            }`}
                           >
-                            <path
-                              d="M19 7a1 1 0 0 0-1 1v11.191A1.92 1.92 0 0 1 15.99 21H8.01A1.92 1.92 0 0 1 6 19.191V8a1 1 0 0 0-2 0v11.191A3.918 3.918 0 0 0 8.01 23h7.98A3.918 3.918 0 0 0 20 19.191V8a1 1 0 0 0-1-1Zm1-3h-4V2a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v2H4a1 1 0 0 0 0 2h16a1 1 0 0 0 0-2ZM10 4V3h4v1Z"
-                              data-original="#000000"
-                            />
-                            <path
-                              d="M11 17v-7a1 1 0 0 0-2 0v7a1 1 0 0 0 2 0Zm4 0v-7a1 1 0 0 0-2 0v7a1 1 0 0 0 2 0Z"
-                              data-original="#000000"
-                            />
-                          </svg>
-                        </button>
-                      </td>
-                    </tr>
-                  );
-                })}
+                            {application?.fee}
+                          </td>
+                          <td className="py-2 px-1 flex flex-col justify-center items-center gap-2 text-sm capitalize">
+                            <button
+                              className="mr-4 btn"
+                              title="Delete"
+                              onClick={() =>
+                                handleDeleteApplication(application)
+                              }
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="w-5 fill-red-500 hover:fill-red-700"
+                                viewBox="0 0 24 24"
+                              >
+                                <path
+                                  d="M19 7a1 1 0 0 0-1 1v11.191A1.92 1.92 0 0 1 15.99 21H8.01A1.92 1.92 0 0 1 6 19.191V8a1 1 0 0 0-2 0v11.191A3.918 3.918 0 0 0 8.01 23h7.98A3.918 3.918 0 0 0 20 19.191V8a1 1 0 0 0-1-1Zm1-3h-4V2a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v2H4a1 1 0 0 0 0 2h16a1 1 0 0 0 0-2ZM10 4V3h4v1Z"
+                                  data-original="#000000"
+                                />
+                                <path
+                                  d="M11 17v-7a1 1 0 0 0-2 0v7a1 1 0 0 0 2 0Zm4 0v-7a1 1 0 0 0-2 0v7a1 1 0 0 0 2 0Z"
+                                  data-original="#000000"
+                                />
+                              </svg>
+                            </button>
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </>
+                )}
               </tbody>
             </table>
           </div>
