8c8a83cc83bba5d806a00fc76e209ff5e460a955
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-11 08:21:13 +0600

    update user overview dashboard

--- src/pages/Dashboard/UserDashboard/UserOverview/UserOverview.jsx
+++ src/pages/Dashboard/UserDashboard/UserOverview/UserOverview.jsx
@@ -3,7 +3,7 @@ import Loading from "../../../../Components/Shared/Loading/Loading";
 import useApplication from "../../../../hooks/useApplication";
 
 const UserOverview = () => {
-  const [application,,isPending] = useApplication();
+  const [application, , isPending] = useApplication();
   //  const [paidApplication,setpaidApplication] = useState()
   const paidApplication = application?.filter((data) => data.fee === "paid");
   const unPaidApplication = application?.filter(
@@ -30,111 +30,54 @@ const UserOverview = () => {
 
   return (
     <div className="">
-      <DashboardTitle title={"My Dashboard Overview"}/>
+      <DashboardTitle title={"My Dashboard Overview"} />
 
       {/* User-specific overview cards */}
       <section className="grid grid-cols-1 md:grid-cols-2 gap-6">
         {/* Pending Applications */}
-        <div className="bg-lightTeal shadow rounded-lg p-10">
-          <h3
-            className="font-semibold text-darkGreen text-xl pb-5 "
-          >
+        <div className="bg-gradient-to-r from-teal to-green-600 shadow rounded-lg p-10">
+          <h3 className="font-semibold text-darkGreen text-xl pb-5">
             My Pending Applications
           </h3>
           <p className="text-white text-lg pl-4">{application?.length}</p>
         </div>
 
         {/* Verified Certificates */}
-        <div className="bg-lightTeal shadow rounded-lg p-10">
-          <h3
-            className="font-semibold text-darkGreen text-xl pb-5 "
-          >
+        <div className="bg-gradient-to-r from-blue-400 to-indigo-600 shadow rounded-lg p-10">
+          <h3 className="font-semibold text-darkGreen text-xl pb-5">
             Verified Certificates
           </h3>
-          <p className="text-white text-lg pl-4">{verifiedApplication?.length}</p>
+          <p className="text-white text-lg pl-4">
+            {verifiedApplication?.length}
+          </p>
         </div>
 
         {/* Rejected Certificates */}
-        <div className="bg-lightTeal shadow rounded-lg p-10">
-          <h3
-            className="font-semibold text-darkGreen text-xl pb-5 "
-          >
+        <div className="bg-gradient-to-r from-red-400 to-red-700 shadow rounded-lg p-10">
+          <h3 className="font-semibold text-darkGreen text-xl pb-5">
             Rejected Certificates
           </h3>
-          <p className="text-white text-lg pl-4">{rejectedApplication?.length}</p>
+          <p className="text-white text-lg pl-4">
+            {rejectedApplication?.length}
+          </p>
         </div>
 
         {/* Total Paid */}
-        <div className="bg-lightTeal shadow rounded-lg p-10">
-          <h3
-            className="font-semibold text-darkGreen text-xl pb-5 "
-          >
+        <div className="bg-gradient-to-r from-purple-500 to-purple-700 shadow rounded-lg p-10">
+          <h3 className="font-semibold text-darkGreen text-xl pb-5">
             Total Paid
           </h3>
           <p className="text-white text-lg pl-4">&#x9F3; {totalPaid}</p>
         </div>
 
         {/* Total Due */}
-        <div className="bg-lightTeal shadow rounded-lg p-10">
-          <h3
-            className="font-semibold text-darkGreen text-xl pb-5 "
-          >
+        <div className="bg-gradient-to-r from-yellow-500 to-orange-500 shadow rounded-lg p-10">
+          <h3 className="font-semibold text-darkGreen text-xl pb-5">
             Total Due
           </h3>
           <p className="text-white text-lg pl-4">&#x9F3; {totalUnPaid}</p>
         </div>
       </section>
-      {/* Recent Activity
-      <section className="mt-8">
-        <h3 className="font-semibold mb-4 text-lg">Recent Activity</h3>
-
-        <div className="bg-white shadow rounded-lg border border-red-600">
-          <div className="overflow-x-auto">
-            <table className="min-w-full table-auto">
-              <thead className="bg-lightTeal text-white uppercase">
-                <tr>
-                  <th className="text-left p-2 md:p-4 font-semibold text-xs md:text-sm lg:text-base w-1/4">
-                    Certificate Type
-                  </th>
-                  <th className="text-left p-2 md:p-4 font-semibold text-xs md:text-sm lg:text-base w-1/4">
-                    Application
-                  </th>
-                  <th className="text-left p-2 md:p-4 font-semibold text-xs md:text-sm lg:text-base w-1/4">
-                    Status
-                  </th>
-                  <th className="text-left p-2 md:p-4 font-semibold text-xs md:text-sm lg:text-base w-1/4">
-                    Application Date
-                  </th>
-                  <th className="text-left p-2 md:p-4 font-semibold text-xs md:text-sm lg:text-base w-1/4">
-                    Publish Date
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {application?.map((app) => (
-                  <tr key={app?._id} className="border-b">
-                    <td className="p-2 md:p-4 text-xs md:text-sm lg:text-base uppercase overflow-hidden text-ellipsis whitespace-nowrap">
-                      {app?.certificateType}
-                    </td>
-                    <td className="p-2 md:p-4 text-xs md:text-sm lg:text-base overflow-hidden text-ellipsis whitespace-nowrap">
-                      {app?.name}
-                    </td>
-                    <td className="p-2 md:p-4 text-xs md:text-sm lg:text-base overflow-hidden text-ellipsis whitespace-nowrap">
-                      {app?.Status}
-                    </td>
-                    <td className="p-2 md:p-4 text-xs md:text-sm lg:text-base overflow-hidden text-ellipsis whitespace-nowrap">
-                      {formatDate(app?.ApplyDate)}
-                    </td>
-                    <td className="p-2 md:p-4 text-xs md:text-sm lg:text-base overflow-hidden text-ellipsis whitespace-nowrap">
-                      {formatDate(app?.publishDate)}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </section> */}
     </div>
   );
 };
