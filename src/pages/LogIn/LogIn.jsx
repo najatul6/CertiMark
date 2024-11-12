9aa7088d13f29560a50b636627c02f647b0dec0e
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-12 21:42:34 +0600

    create a custom css for log in page

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
-
+import
 const LogIn = () => {
   const {
     register,
