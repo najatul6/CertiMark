a73de340a30c4039bf4d15bdd69a6796cbbdeb21
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-12 22:01:01 +0600

    add toast in contact page

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,4 +1,5 @@
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 
 const ContactUs = () => {
   const {
@@ -10,7 +11,7 @@ const ContactUs = () => {
   const onSubmit = (data) => {
     // TODO: Implement form submission logic here (e.g., API call)
     console.log("Contact form submitted:", data);
-    alert("Your message has been sent!");
+    swal.success("Your message has been sent!");
   };
 
   return (
