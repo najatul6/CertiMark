aaf91897e32739fd48f12f3ff6f94c26d2aa7d27
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-13 23:35:36 +0600

    add checkout form in payment page

--- src/pages/Dashboard/UserDashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/UserDashboard/Payment/Payment.jsx
@@ -1,19 +1,20 @@
-import { loadStripe } from "@stripe/stripe-js"
-import DashboardTitle from "../../../../Components/Shared/DashboardTitle/DashboardTitle"
-import { Elements } from "@stripe/react-stripe-js"
+import { loadStripe } from "@stripe/stripe-js";
+import DashboardTitle from "../../../../Components/Shared/DashboardTitle/DashboardTitle";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckOutForm from "./CheckOutForm";
 
-const stripePromise=loadStripe(import.meta.env.VITE_PAYMENT_PK)
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_PK);
 const Payment = () => {
   return (
     <div>
-          <DashboardTitle title={"Payment"} />
-          <div>
-            <Elements stripe={stripePromise}>
-
-            </Elements>
-          </div>
+      <DashboardTitle title={"Payment"} />
+      <div>
+        <Elements stripe={stripePromise}>
+          <CheckOutForm />
+        </Elements>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Payment
+export default Payment;
