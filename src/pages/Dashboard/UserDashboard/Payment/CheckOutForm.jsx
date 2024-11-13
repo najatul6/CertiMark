383fb21080bc0e2c84a8c30399e15cd4e871e9de
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-13 23:17:50 +0600

    add some style in stripe form

--- src/pages/Dashboard/UserDashboard/Payment/CheckOutForm.jsx
+++ src/pages/Dashboard/UserDashboard/Payment/CheckOutForm.jsx
@@ -1,17 +1,31 @@
-import { CardElement } from "@stripe/react-stripe-js"
+import { CardElement } from "@stripe/react-stripe-js";
 
 const CheckOutForm = () => {
-    const handleSubmit = async(e)=>{
-        e.preventDefault()
-
-    }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
   return (
-    <form onSubmit={handleSubmit }>
-<CardElement>
-
-</CardElement>
+    <form onSubmit={handleSubmit}>
+      <CardElement  options={{
+          style: {
+            base: {
+              fontSize: '16px',
+              color: '#424770',
+              '::placeholder': {
+                color: '#aab7c4',
+              },
+            },
+            invalid: {
+              color: '#9e2146',
+            },
+          },
+        }}>
+      <button type="submit" disabled={!stripe}>
+        Pay
+      </button>
+      </CardElement>
     </form>
-  )
-}
+  );
+};
 
-export default CheckOutForm
+export default CheckOutForm;
