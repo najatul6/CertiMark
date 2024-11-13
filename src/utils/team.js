80887d7273390634fc2351ac2ea705ccfbf8fc13
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-14 03:36:30 +0600

    add some teams data

---
+++ src/utils/team.js
@@ -0,0 +1,102 @@
+import najatul from '../assets/Team/Najatul_islam.jpg'
+export const teams=[
+    {
+        "_id":'672abca0618b4430ee3fb5ffC',
+        "Name":"Md. Najatul Islam",
+        "Email":"najatulislam11@gmail.com",
+        "profession":"Team Leader",
+        "skill":"Full Stack Developer",
+        "image":`${najatul}`,
+        "fbLink":"https://www.facebook.com/najatul6/",
+        "github":"https://github.com/najatul6",
+        "linkedin":"https://www.linkedin.com/in/najatul-islam-94193b1b3/"
+    },
+    {
+        "_id":'672abca0618b4430ee3fb5ffC',
+        "Name":"Md. Najatul Islam",
+        "Email":"najatulislam11@gmail.com",
+        "profession":"Team Leader",
+        "skill":"Full Stack Developer",
+        "image":`${najatul}`,
+        "fbLink":"https://www.facebook.com/najatul6/",
+        "github":"https://github.com/najatul6",
+        "linkedin":"https://www.linkedin.com/in/najatul-islam-94193b1b3/"
+    },
+    {
+        "_id":'672abca0618b4430ee3fb5ffC',
+        "Name":"Md. Najatul Islam",
+        "Email":"najatulislam11@gmail.com",
+        "profession":"Team Leader",
+        "skill":"Full Stack Developer",
+        "image":`${najatul}`,
+        "fbLink":"https://www.facebook.com/najatul6/",
+        "github":"https://github.com/najatul6",
+        "linkedin":"https://www.linkedin.com/in/najatul-islam-94193b1b3/"
+    },
+    {
+        "_id":'672abca0618b4430ee3fb5ffC',
+        "Name":"Md. Najatul Islam",
+        "Email":"najatulislam11@gmail.com",
+        "profession":"Team Leader",
+        "skill":"Full Stack Developer",
+        "image":`${najatul}`,
+        "fbLink":"https://www.facebook.com/najatul6/",
+        "github":"https://github.com/najatul6",
+        "linkedin":"https://www.linkedin.com/in/najatul-islam-94193b1b3/"
+    },
+    {
+        "_id":'672abca0618b4430ee3fb5ffC',
+        "Name":"Md. Najatul Islam",
+        "Email":"najatulislam11@gmail.com",
+        "profession":"Team Leader",
+        "skill":"Full Stack Developer",
+        "image":`${najatul}`,
+        "fbLink":"https://www.facebook.com/najatul6/",
+        "github":"https://github.com/najatul6",
+        "linkedin":"https://www.linkedin.com/in/najatul-islam-94193b1b3/"
+    },
+    {
+        "_id":'672abca0618b4430ee3fb5ffC',
+        "Name":"Md. Najatul Islam",
+        "Email":"najatulislam11@gmail.com",
+        "profession":"Team Leader",
+        "skill":"Full Stack Developer",
+        "image":`${najatul}`,
+        "fbLink":"https://www.facebook.com/najatul6/",
+        "github":"https://github.com/najatul6",
+        "linkedin":"https://www.linkedin.com/in/najatul-islam-94193b1b3/"
+    },
+    {
+        "_id":'672abca0618b4430ee3fb5ffC',
+        "Name":"Md. Najatul Islam",
+        "Email":"najatulislam11@gmail.com",
+        "profession":"Team Leader",
+        "skill":"Full Stack Developer",
+        "image":`${najatul}`,
+        "fbLink":"https://www.facebook.com/najatul6/",
+        "github":"https://github.com/najatul6",
+        "linkedin":"https://www.linkedin.com/in/najatul-islam-94193b1b3/"
+    },
+    {
+        "_id":'672abca0618b4430ee3fb5ffC',
+        "Name":"Md. Najatul Islam",
+        "Email":"najatulislam11@gmail.com",
+        "profession":"Team Leader",
+        "skill":"Full Stack Developer",
+        "image":`${najatul}`,
+        "fbLink":"https://www.facebook.com/najatul6/",
+        "github":"https://github.com/najatul6",
+        "linkedin":"https://www.linkedin.com/in/najatul-islam-94193b1b3/"
+    },
+    {
+        "_id":'672abca0618b4430ee3fb5ffC',
+        "Name":"Md. Najatul Islam",
+        "Email":"najatulislam11@gmail.com",
+        "profession":"Team Leader",
+        "skill":"Full Stack Developer",
+        "image":`${najatul}`,
+        "fbLink":"https://www.facebook.com/najatul6/",
+        "github":"https://github.com/najatul6",
+        "linkedin":"https://www.linkedin.com/in/najatul-islam-94193b1b3/"
+    },
+]
