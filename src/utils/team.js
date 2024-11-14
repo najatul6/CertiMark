0714b355b3093e77dcd0d94138af796004cce939
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-14 23:12:52 +0600

    update profile picture

--- src/utils/team.js
+++ src/utils/team.js
@@ -4,6 +4,7 @@ import atikur from '../assets/Team/atikur.png'
 import hridoy from '../assets/Team/hridoy.png'
 import jwel from '../assets/Team/Jwel.png'
 import ashikur from '../assets/Team/ashikur.png'
+import fahim from '../assets/Team/fahim.png'
 
 // female 
 import israt from '../assets/Team/Israt.png'
@@ -75,6 +76,17 @@ export const teams=[
         "profession":"Team Leader",
         "skill":"Full Stack Developer",
         "image":`${aziz}`,
+        "fbLink":"https://www.facebook.com/najatul6/",
+        "github":"https://github.com/najatul6",
+        "linkedin":"https://www.linkedin.com/in/najatul-islam-94193b1b3/"
+    },
+    {
+        "_id":'672abca618b4430ee3fb5ffC',
+        "Name":"Md. Najatul Islam",
+        "Email":"najatulislam11@gmail.com",
+        "profession":"Team Leader",
+        "skill":"Full Stack Developer",
+        "image":`${fahim}`,
         "fbLink":"https://www.facebook.com/najatul6/",
         "github":"https://github.com/najatul6",
         "linkedin":"https://www.linkedin.com/in/najatul-islam-94193b1b3/"
