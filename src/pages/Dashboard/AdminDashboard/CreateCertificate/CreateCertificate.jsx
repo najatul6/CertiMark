6a30b72517a5b49c49d5523311db2adaff4dbefb
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-16 23:12:23 +0600

    get full application data

--- src/pages/Dashboard/AdminDashboard/CreateCertificate/CreateCertificate.jsx
+++ src/pages/Dashboard/AdminDashboard/CreateCertificate/CreateCertificate.jsx
@@ -1,12 +1,17 @@
 import { useParams } from "react-router"
 import DashboardTitle from "../../../../Components/Shared/DashboardTitle/DashboardTitle"
 import useAxiosSecure from "../../../../hooks/useAxiosSecure"
+import { useEffect } from "react"
 
 const CreateCertificate = () => {
     const {applicationId}=useParams()
     const axiosSecure=useAxiosSecure()
-    
-    console.log(applicationId);
+    useEffect(()=>{
+        axiosSecure.get(`/applicants/${applicationId}`)
+        .then(res=>{
+            console.log(res.data);
+        })
+    },[applicationId,axiosSecure])
 
   return (
     <div>
