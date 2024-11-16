66d086ffc91759707fa9150a0c8f126e16bb7c86
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-17 02:55:38 +0600

    update pending application page layout

--- src/pages/Dashboard/AdminDashboard/CreateCertificate/CreateCertificate.jsx
+++ src/pages/Dashboard/AdminDashboard/CreateCertificate/CreateCertificate.jsx
@@ -4,6 +4,8 @@ import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 import { useEffect, useState } from "react";
 import testimonial from "../../../../assets/cartificates/Testimonial.png";
 import clearnes from "../../../../assets/cartificates/clearanceCertificate .jpg";
+import compared from "../../../../assets/cartificates/compared.png";
+import controller from "../../../../assets/cartificates/controller.png";
 
 const CreateCertificate = () => {
   const { applicationId } = useParams();
@@ -19,9 +21,12 @@ const CreateCertificate = () => {
   return (
     <div>
       <DashboardTitle title={"Create Certificate"} />
-      <div>
+      <div className="grid grid-cols-2 place-items-center">
+        <div className="w-full bg-white h-full">
+            <form></form>
+        </div>
         <div className="relative">
-            {/* Image  */}
+          {/* Image  */}
           {application?.certificateType === "testimonial" ? (
             <div className="w-[560px] h-[400px]">
               <img
@@ -46,46 +51,83 @@ const CreateCertificate = () => {
             ) : (
               <div className="relative w-[560px] h-[400px]">
                 {/* Serial Number  */}
-                <h6 className="absolute top-[108px] left-[85px] tracking-[3px] font-montserrat text-[9px] font-bold text-blackDiamond">26458589</h6>
+                <h6 className="absolute top-[108px] left-[85px] tracking-[3px] font-montserrat text-[9px] font-bold text-blackDiamond">
+                  26458589
+                </h6>
 
                 {/* Year  */}
-                <h2 className="absolute top-[90px] left-[405px] font-tenor text-blackDiamond">2024</h2>
+                <h2 className="absolute top-[90px] left-[405px] font-tenor text-blackDiamond">
+                  2024
+                </h2>
 
                 {/* Registration Number  */}
-                <h6 className="absolute top-[125px] right-[57px] font-playwrite text-[7px] font-bold text-blackDiamond">1502041769</h6>
+                <h6 className="absolute top-[125px] right-[57px] font-playwrite text-[7px] font-bold text-blackDiamond">
+                  1502041769
+                </h6>
 
                 {/* Session  */}
-                <h6 className="absolute top-[145px] right-[80px] font-playwrite text-[7px] font-bold text-blackDiamond">2019-20</h6>
+                <h6 className="absolute top-[145px] right-[80px] font-playwrite text-[7px] font-bold text-blackDiamond">
+                  2019-20
+                </h6>
 
                 {/* Student Name  */}
-                <h6 className="absolute capitalize top-[160px] right-[178px] font-playwrite text-[12px] font-bold text-blackDiamond">Md. Najatul Islam</h6>
+                <h6 className="absolute capitalize top-[160px] right-[178px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                  Md. Najatul Islam
+                </h6>
 
                 {/* Father Name  */}
-                <h6 className="absolute capitalize top-[182px] right-[165px] font-playwrite text-[12px] font-bold text-blackDiamond">Md. Mozibur Rahman</h6>
+                <h6 className="absolute capitalize top-[182px] right-[165px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                  Md. Mozibur Rahman
+                </h6>
 
                 {/* Mother Name  */}
-                <h6 className="absolute capitalize top-[202px] right-[162px] font-playwrite text-[12px] font-bold text-blackDiamond">Mst. Khadija begum</h6>
+                <h6 className="absolute capitalize top-[202px] right-[162px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                  Mst. Khadija begum
+                </h6>
 
                 {/* College Name  */}
-                <h6 className="absolute capitalize top-[222px] right-[120px] font-playwrite text-[12px] font-bold text-blackDiamond">Rangpur Polytechnic Institute, Rangpur</h6>
+                <h6 className="absolute capitalize top-[222px] right-[120px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                  Rangpur Polytechnic Institute, Rangpur
+                </h6>
 
                 {/* Student Roll  */}
-                <h6 className="absolute capitalize top-[250px] left-[127px] font-playwrite text-[10px] font-bold text-blackDiamond">490862</h6>
+                <h6 className="absolute capitalize top-[250px] left-[127px] font-playwrite text-[10px] font-bold text-blackDiamond">
+                  490862
+                </h6>
 
                 {/* Technology  */}
-                <h6 className="absolute capitalize top-[266px] left-[180px] font-playwrite text-[12px] font-bold text-blackDiamond">Computer Technology</h6>
+                <h6 className="absolute capitalize top-[266px] left-[180px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                  Computer Technology
+                </h6>
 
                 {/* Held of Month  */}
-                <h6 className="absolute capitalize top-[293px] left-[65px] font-playwrite text-[10px] font-bold text-blackDiamond">December,2019 - February, 2025</h6>
+                <h6 className="absolute capitalize top-[293px] left-[65px] font-playwrite text-[10px] font-bold text-blackDiamond">
+                  December,2019 - February, 2025
+                </h6>
 
                 {/* Result  */}
-                <h6 className="absolute capitalize top-[294px] right-[157px] font-playwrite text-[10px] font-bold text-blackDiamond">3.72</h6>
+                <h6 className="absolute capitalize top-[294px] right-[157px] font-playwrite text-[10px] font-bold text-blackDiamond">
+                  3.72
+                </h6>
 
                 {/* Publication of Result  */}
-                <h6 className="absolute capitalize top-[345px] left-[175px] font-playwrite text-[8px] font-bold text-blackDiamond">May 12, 2025</h6>
+                <h6 className="absolute capitalize top-[345px] left-[175px] font-playwrite text-[8px] font-bold text-blackDiamond">
+                  May 12, 2025
+                </h6>
 
                 {/* Date of issue  */}
-                <h6 className="absolute capitalize top-[362px] left-[110px] font-playwrite text-[8px] font-bold text-blackDiamond">November 11, 2025</h6>
+                <h6 className="absolute capitalize top-[362px] left-[110px] font-playwrite text-[8px] font-bold text-blackDiamond">
+                  November 11, 2025
+                </h6>
+                {/* Compared By  */}
+                <img
+                  src={compared}
+                  alt="Comapred signature"
+                  className="absolute bottom-[33px] left-[266px] w-[75px]"
+                />
+
+                {/* Compared By  */}
+                <img src={controller} alt="Controller Signature" className="absolute bottom-[35px] right-[85px] w-[75px]"/>
               </div>
             )}
           </div>
