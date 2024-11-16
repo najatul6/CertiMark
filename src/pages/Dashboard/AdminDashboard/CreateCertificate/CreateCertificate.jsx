f6697a206f5b968a1cad321230213e7030336979
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-16 23:20:52 +0600

    show certificate conditionally

--- src/pages/Dashboard/AdminDashboard/CreateCertificate/CreateCertificate.jsx
+++ src/pages/Dashboard/AdminDashboard/CreateCertificate/CreateCertificate.jsx
@@ -1,28 +1,36 @@
-import { useParams } from "react-router"
-import DashboardTitle from "../../../../Components/Shared/DashboardTitle/DashboardTitle"
-import useAxiosSecure from "../../../../hooks/useAxiosSecure"
-import { useEffect } from "react"
-import testimonial from '../../../../assets/cartificates/Testimonial.png'
-import clearnes from '../../../../assets/cartificates/clearanceCertificate .jpg'
+import { useParams } from "react-router";
+import DashboardTitle from "../../../../Components/Shared/DashboardTitle/DashboardTitle";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+import { useEffect, useState } from "react";
+import testimonial from "../../../../assets/cartificates/Testimonial.png";
+import clearnes from "../../../../assets/cartificates/clearanceCertificate .jpg";
 
 const CreateCertificate = () => {
-    const {applicationId}=useParams()
-    const axiosSecure=useAxiosSecure()
-    useEffect(()=>{
-        axiosSecure.get(`/applicants/${applicationId}`)
-        .then(res=>{
-            console.log(res.data);
-        })
-    },[applicationId,axiosSecure])
+  const { applicationId } = useParams();
+  const axiosSecure = useAxiosSecure();
+  const [application, setApplication] = useState({});
+  useEffect(() => {
+    axiosSecure.get(`/applicants/${applicationId}`).then((res) => {
+      setApplication(res?.data);
+      console.log(res.data);
+    });
+  }, [applicationId, axiosSecure]);
 
   return (
     <div>
-        <DashboardTitle title={"Create Certificate"} />
-    <div>
-
+      <DashboardTitle title={"Create Certificate"} />
+      <div></div>
+      {application?.certificateType === "testimonial" ? (
+        <div>
+          <img src={testimonial} alt={`${application.name}'s certificate`} />
+        </div>
+      ) : (
+        <div>
+            <img src={clearnes} alt="" />
+        </div>
+      )}
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default CreateCertificate
+export default CreateCertificate;
