8825b7c4916fd51f981217609a4f574ba0627dea
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-17 11:06:59 +0600

    generate image successfully and upload it img bb

---
+++ src/Components/CertificateCreatetion/ClearanceForm.jsx
@@ -0,0 +1,170 @@
+
+const ClearanceForm = () => {
+  return (
+    <div>
+        <div className="absolute top-0 left-0 w-full h-full">
+            {application?.certificateType === "testimonial" ? (
+              // For Testimonial
+              <div className="relative w-[560px] h-[400px]">
+                {/* Student Name  */}
+                <h6 className="absolute uppercase top-[145px] left-[128px] tracking-[2px] font-serif text-[12px] font-[800] italic text-blackDiamond">
+                  Md. Najatul Islam
+                </h6>
+
+                {/* Father Name  */}
+                {/* <h6 className="absolute capitalize top-[182px] right-[165px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                  Md. Mozibur Rahman
+                </h6> */}
+
+                {/* Mother Name  */}
+                {/* <h6 className="absolute capitalize top-[202px] right-[162px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                  Mst. Khadija begum
+                </h6> */}
+
+                {/* Student Roll  */}
+                {/* <h6 className="absolute capitalize top-[250px] left-[127px] font-playwrite text-[10px] font-bold text-blackDiamond">
+                  490862
+                </h6> */}
+
+                {/* Registration Number  */}
+                {/* <h6 className="absolute top-[125px] right-[57px] font-playwrite text-[7px] font-bold text-blackDiamond">
+                1502041769
+              </h6> */}
+
+                {/* Session  */}
+                {/* <h6 className="absolute top-[145px] right-[80px] font-playwrite text-[7px] font-bold text-blackDiamond">
+                2019-20
+              </h6> */}
+
+                {/* Technology  */}
+                {/* <h6 className="absolute capitalize top-[266px] left-[180px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                  Computer Technology
+                </h6> */}
+
+                {/* Held of Month  */}
+                {/* <h6 className="absolute capitalize top-[293px] left-[65px] font-playwrite text-[10px] font-bold text-blackDiamond">
+                  December,2019 - February, 2025
+                </h6> */}
+
+                {/* Board Name  */}
+                {/* <h6 className="absolute capitalize top-[222px] right-[120px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                  Rangpur Polytechnic Institute, Rangpur
+                </h6> */}
+
+                {/* Result  */}
+                {/* <h6 className="absolute capitalize top-[294px] right-[157px] font-playwrite text-[10px] font-bold text-blackDiamond">
+                  3.72
+                </h6> */}
+
+                {/* Date of issue  */}
+                {/* <h6 className="absolute capitalize top-[362px] left-[110px] font-playwrite text-[8px] font-bold text-blackDiamond">
+                  November 11, 2025
+                </h6> */}
+
+                {/* Register Signature  */}
+                {/* <img
+                  src={compared}
+                  alt="Comapred signature"
+                  className="absolute bottom-[33px] left-[266px] w-[75px]"
+                /> */}
+
+                {/* Principal Signature  */}
+                {/* <img
+                  src={controller}
+                  alt="Controller Signature"
+                  className="absolute bottom-[35px] right-[85px] w-[75px]"
+                /> */}
+              </div>
+            ) : (
+              // For Clearance
+              <div className="relative w-[560px] h-[400px]">
+                {/* Serial Number  */}
+                <h6 className="absolute top-[108px] left-[85px] tracking-[3px] font-montserrat text-[9px] font-bold text-blackDiamond">
+                  {serialNumber}
+                </h6>
+
+                {/* Year  */}
+                <h2 className="absolute top-[90px] left-[405px] font-tenor text-blackDiamond">
+                  2024
+                </h2>
+
+                {/* Registration Number  */}
+                <h6 className="absolute top-[125px] right-[57px] font-playwrite text-[7px] font-bold text-blackDiamond">
+                  {application?.registrationNo}
+                </h6>
+
+                {/* Session  */}
+                <h6 className="absolute top-[145px] right-[80px] font-playwrite text-[7px] font-bold text-blackDiamond">
+                  {application?.yearOfCompletion}
+                </h6>
+
+                {/* Student Name  */}
+                <h6 className="absolute capitalize top-[160px] right-[178px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                  {application?.name}
+                </h6>
+
+                {/* Father Name  */}
+                <h6 className="absolute capitalize top-[182px] right-[165px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                  {application?.fatherName}
+                </h6>
+
+                {/* Mother Name  */}
+                <h6 className="absolute capitalize top-[202px] right-[162px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                  {application?.motherName}
+                </h6>
+
+                {/* College Name  */}
+                <h6 className="absolute capitalize top-[222px] right-[120px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                  Rangpur Polytechnic Institute, Rangpur
+                </h6>
+
+                {/* Student Roll  */}
+                <h6 className="absolute capitalize top-[250px] left-[127px] font-playwrite text-[10px] font-bold text-blackDiamond">
+                  {application?.studentId}
+                </h6>
+
+                {/* Technology  */}
+                <h6 className="absolute capitalize top-[266px] left-[180px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                  {application?.technology}
+                </h6>
+
+                {/* Held of Month  */}
+                <h6 className="absolute capitalize top-[293px] left-[65px] font-playwrite text-[10px] font-bold text-blackDiamond">
+                  {heldOnMonth}
+                </h6>
+
+                {/* Result  */}
+                <h6 className="absolute capitalize top-[294px] right-[157px] font-playwrite text-[10px] font-bold text-blackDiamond">
+                  {result}
+                </h6>
+
+                {/* Publication of Result  */}
+                <h6 className="absolute capitalize top-[345px] left-[175px] font-playwrite text-[8px] font-bold text-blackDiamond">
+                  {publishDate}
+                </h6>
+
+                {/* Date of issue  */}
+                <h6 className="absolute capitalize top-[362px] left-[110px] font-playwrite text-[8px] font-bold text-blackDiamond">
+                  {dateOfIssue}
+                </h6>
+                {/* Compared By  */}
+                <img
+                  src={compared}
+                  alt="Comapred signature"
+                  className="absolute bottom-[33px] left-[266px] w-[75px]"
+                />
+
+                {/* Compared By  */}
+                <img
+                  src={controller}
+                  alt="Controller Signature"
+                  className="absolute bottom-[35px] right-[85px] w-[75px]"
+                />
+              </div>
+            )}
+          </div>
+    </div>
+  )
+}
+
+export default ClearanceForm
--- src/pages/Dashboard/AdminDashboard/CreateCertificate/CreateCertificate.jsx
+++ src/pages/Dashboard/AdminDashboard/CreateCertificate/CreateCertificate.jsx
@@ -1,24 +1,27 @@
 import { useParams } from "react-router";
 import DashboardTitle from "../../../../Components/Shared/DashboardTitle/DashboardTitle";
 import useAxiosSecure from "../../../../hooks/useAxiosSecure";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import testimonial from "../../../../assets/cartificates/Testimonial.png";
 import clearnes from "../../../../assets/cartificates/clearanceCertificate .jpg";
 import compared from "../../../../assets/cartificates/compared.png";
 import controller from "../../../../assets/cartificates/controller.png";
 import { TbFidgetSpinner } from "react-icons/tb";
+import { toPng } from "html-to-image";
+import { imageUpload } from "../../../../utils/imageUpload";
+import toast from "react-hot-toast";
 
 const CreateCertificate = () => {
   const { applicationId } = useParams();
   const axiosSecure = useAxiosSecure();
-  const [isLoading, setIsLoading]=useState(false)
+  const [isLoading, setIsLoading] = useState(false);
   const [application, setApplication] = useState({});
   const [serialNumber, setSerialNumber] = useState("");
   const [heldOnMonth, setHeldOnMonth] = useState("");
   const [result, setResult] = useState("");
   const [publishDate, setPublishDate] = useState("");
   const [dateOfIssue, setDateOfIssue] = useState("");
-
+  const ref = useRef(null);
   useEffect(() => {
     axiosSecure.get(`/applicants/${applicationId}`).then((res) => {
       setApplication(res?.data);
@@ -26,6 +29,48 @@ const CreateCertificate = () => {
     });
   }, [applicationId, axiosSecure]);
 
+  
+  const onButtonClick = useCallback(() => {
+    if (ref.current === null) {
+      return;
+    }
+    setIsLoading(true);
+
+    // Generate high-quality image
+    toPng(ref.current, { cacheBust: true, quality: 1, pixelRatio: 2 })
+      .then((dataUrl) => {
+
+        // Convert base64 data URL to a Blob
+        const blob = dataUrlToBlob(dataUrl);
+
+        // Upload to ImgBB using the provided imageUpload function
+        imageUpload(blob)
+          .then((response) => {
+            console.log('Uploaded Successfully:', response.data.url);
+            toast.success('Uploaded Successfully:');
+            setIsLoading(false);
+          })
+          .catch((error) => {
+            console.error('Upload Failed:', error);
+            setIsLoading(false);
+          });
+      })
+      .catch((err) => {
+        console.error('Image Generation Failed:', err);
+        setIsLoading(false);
+      });
+  }, [ref]);
+
+  const dataUrlToBlob = (dataUrl) => {
+    const byteString = atob(dataUrl.split(',')[1]);
+    const mimeString = dataUrl.split(',')[0].split(':')[1].split(';')[0];
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+    return new Blob([ab], { type: mimeString });
+  };
   return (
     <div>
       <DashboardTitle title={"Create Certificate"} />
@@ -133,7 +178,7 @@ const CreateCertificate = () => {
         <div className="relative">
           {/* Image  */}
           {application?.certificateType === "testimonial" ? (
-            // Testimonial Image 
+            // Testimonial Image
             <div className="w-[560px] h-[400px]">
               <img
                 src={testimonial}
@@ -142,189 +187,117 @@ const CreateCertificate = () => {
               />
             </div>
           ) : (
-            // Clearance Image 
-            <div className="w-[560px] h-[400px]">
-              <img
-                src={clearnes}
-                alt={`${application.name}'s certificate`}
-                className="w-[560px] h-[426px]"
-              />
-            </div>
+            // Clearance
+            <>
+              <div ref={ref} className="relative">
+                <div className="absolute w-[560px] h-[400px]">
+                  {/* Serial Number  */}
+                  <h6 className="absolute top-[108px] left-[85px] tracking-[3px] font-montserrat text-[9px] font-bold text-blackDiamond">
+                    {serialNumber}
+                  </h6>
+
+                  {/* Year  */}
+                  <h2 className="absolute top-[90px] left-[405px] font-tenor text-blackDiamond">
+                    2024
+                  </h2>
+
+                  {/* Registration Number  */}
+                  <h6 className="absolute top-[125px] right-[57px] font-playwrite text-[7px] font-bold text-blackDiamond">
+                    {application?.registrationNo}
+                  </h6>
+
+                  {/* Session  */}
+                  <h6 className="absolute top-[145px] right-[80px] font-playwrite text-[7px] font-bold text-blackDiamond">
+                    {application?.yearOfCompletion}
+                  </h6>
+
+                  {/* Student Name  */}
+                  <h6 className="absolute capitalize top-[160px] right-[178px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                    {application?.name}
+                  </h6>
+
+                  {/* Father Name  */}
+                  <h6 className="absolute capitalize top-[182px] right-[165px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                    {application?.fatherName}
+                  </h6>
+
+                  {/* Mother Name  */}
+                  <h6 className="absolute capitalize top-[202px] right-[162px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                    {application?.motherName}
+                  </h6>
+
+                  {/* College Name  */}
+                  <h6 className="absolute capitalize top-[222px] right-[120px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                    Rangpur Polytechnic Institute, Rangpur
+                  </h6>
+
+                  {/* Student Roll  */}
+                  <h6 className="absolute capitalize top-[250px] left-[127px] font-playwrite text-[10px] font-bold text-blackDiamond">
+                    {application?.studentId}
+                  </h6>
+
+                  {/* Technology  */}
+                  <h6 className="absolute capitalize top-[266px] left-[180px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                    {application?.technology}
+                  </h6>
+
+                  {/* Held of Month  */}
+                  <h6 className="absolute capitalize top-[293px] left-[65px] font-playwrite text-[10px] font-bold text-blackDiamond">
+                    {heldOnMonth}
+                  </h6>
+
+                  {/* Result  */}
+                  <h6 className="absolute capitalize top-[294px] right-[157px] font-playwrite text-[10px] font-bold text-blackDiamond">
+                    {result}
+                  </h6>
+
+                  {/* Publication of Result  */}
+                  <h6 className="absolute capitalize top-[345px] left-[175px] font-playwrite text-[8px] font-bold text-blackDiamond">
+                    {publishDate}
+                  </h6>
+
+                  {/* Date of issue  */}
+                  <h6 className="absolute capitalize top-[362px] left-[110px] font-playwrite text-[8px] font-bold text-blackDiamond">
+                    {dateOfIssue}
+                  </h6>
+                  {/* Compared By  */}
+                  <img
+                    src={compared}
+                    alt="Comapred signature"
+                    className="absolute bottom-[33px] left-[266px] w-[75px]"
+                  />
+
+                  {/* Compared By  */}
+                  <img
+                    src={controller}
+                    alt="Controller Signature"
+                    className="absolute bottom-[35px] right-[85px] w-[75px]"
+                  />
+                </div>
+                <div className="w-[560px] h-[426px]">
+                  <img
+                    src={clearnes}
+                    alt={`${application.name}'s certificate`}
+                    className="w-[560px] h-[426px]"
+                  />
+                </div>
+              </div>
+              <button
+                type="button"
+                onClick={onButtonClick}
+                className="w-full mt-10 bg-[#3AAFA9] text-white py-3 rounded-lg font-semibold font-montserrat text-lg"
+              >
+                {isLoading ? (
+                  <TbFidgetSpinner className="animate-spin m-auto" />
+                ) : (
+                  "Generate Certificate & Approve"
+                )}
+              </button>
+            </>
           )}
           {/* Content  */}
-          <div className="absolute top-0 left-0 w-full h-full">
-            {application?.certificateType === "testimonial" ? (
-              // For Testimonial
-              <div className="relative w-[560px] h-[400px]">
-                {/* Student Name  */}
-                <h6 className="absolute uppercase top-[145px] left-[128px] tracking-[2px] font-serif text-[12px] font-[800] italic text-blackDiamond">
-                  Md. Najatul Islam
-                </h6>
-
-                {/* Father Name  */}
-                {/* <h6 className="absolute capitalize top-[182px] right-[165px] font-playwrite text-[12px] font-bold text-blackDiamond">
-                  Md. Mozibur Rahman
-                </h6> */}
-
-                {/* Mother Name  */}
-                {/* <h6 className="absolute capitalize top-[202px] right-[162px] font-playwrite text-[12px] font-bold text-blackDiamond">
-                  Mst. Khadija begum
-                </h6> */}
-
-                {/* Student Roll  */}
-                {/* <h6 className="absolute capitalize top-[250px] left-[127px] font-playwrite text-[10px] font-bold text-blackDiamond">
-                  490862
-                </h6> */}
-
-                {/* Registration Number  */}
-                {/* <h6 className="absolute top-[125px] right-[57px] font-playwrite text-[7px] font-bold text-blackDiamond">
-                1502041769
-              </h6> */}
-
-                {/* Session  */}
-                {/* <h6 className="absolute top-[145px] right-[80px] font-playwrite text-[7px] font-bold text-blackDiamond">
-                2019-20
-              </h6> */}
-
-                {/* Technology  */}
-                {/* <h6 className="absolute capitalize top-[266px] left-[180px] font-playwrite text-[12px] font-bold text-blackDiamond">
-                  Computer Technology
-                </h6> */}
-
-                {/* Held of Month  */}
-                {/* <h6 className="absolute capitalize top-[293px] left-[65px] font-playwrite text-[10px] font-bold text-blackDiamond">
-                  December,2019 - February, 2025
-                </h6> */}
-
-                {/* Board Name  */}
-                {/* <h6 className="absolute capitalize top-[222px] right-[120px] font-playwrite text-[12px] font-bold text-blackDiamond">
-                  Rangpur Polytechnic Institute, Rangpur
-                </h6> */}
-
-                {/* Result  */}
-                {/* <h6 className="absolute capitalize top-[294px] right-[157px] font-playwrite text-[10px] font-bold text-blackDiamond">
-                  3.72
-                </h6> */}
-
-                {/* Date of issue  */}
-                {/* <h6 className="absolute capitalize top-[362px] left-[110px] font-playwrite text-[8px] font-bold text-blackDiamond">
-                  November 11, 2025
-                </h6> */}
-
-                {/* Register Signature  */}
-                {/* <img
-                  src={compared}
-                  alt="Comapred signature"
-                  className="absolute bottom-[33px] left-[266px] w-[75px]"
-                /> */}
-
-                {/* Principal Signature  */}
-                {/* <img
-                  src={controller}
-                  alt="Controller Signature"
-                  className="absolute bottom-[35px] right-[85px] w-[75px]"
-                /> */}
-              </div>
-            ) : (
-              // For Clearance
-              <div className="relative w-[560px] h-[400px]">
-                {/* Serial Number  */}
-                <h6 className="absolute top-[108px] left-[85px] tracking-[3px] font-montserrat text-[9px] font-bold text-blackDiamond">
-                  {serialNumber}
-                </h6>
-
-                {/* Year  */}
-                <h2 className="absolute top-[90px] left-[405px] font-tenor text-blackDiamond">
-                  2024
-                </h2>
-
-                {/* Registration Number  */}
-                <h6 className="absolute top-[125px] right-[57px] font-playwrite text-[7px] font-bold text-blackDiamond">
-                  {application?.registrationNo}
-                </h6>
-
-                {/* Session  */}
-                <h6 className="absolute top-[145px] right-[80px] font-playwrite text-[7px] font-bold text-blackDiamond">
-                  {application?.yearOfCompletion}
-                </h6>
-
-                {/* Student Name  */}
-                <h6 className="absolute capitalize top-[160px] right-[178px] font-playwrite text-[12px] font-bold text-blackDiamond">
-                  {application?.name}
-                </h6>
-
-                {/* Father Name  */}
-                <h6 className="absolute capitalize top-[182px] right-[165px] font-playwrite text-[12px] font-bold text-blackDiamond">
-                  {application?.fatherName}
-                </h6>
-
-                {/* Mother Name  */}
-                <h6 className="absolute capitalize top-[202px] right-[162px] font-playwrite text-[12px] font-bold text-blackDiamond">
-                  {application?.motherName}
-                </h6>
-
-                {/* College Name  */}
-                <h6 className="absolute capitalize top-[222px] right-[120px] font-playwrite text-[12px] font-bold text-blackDiamond">
-                  Rangpur Polytechnic Institute, Rangpur
-                </h6>
-
-                {/* Student Roll  */}
-                <h6 className="absolute capitalize top-[250px] left-[127px] font-playwrite text-[10px] font-bold text-blackDiamond">
-                  {application?.studentId}
-                </h6>
-
-                {/* Technology  */}
-                <h6 className="absolute capitalize top-[266px] left-[180px] font-playwrite text-[12px] font-bold text-blackDiamond">
-                  {application?.technology}
-                </h6>
-
-                {/* Held of Month  */}
-                <h6 className="absolute capitalize top-[293px] left-[65px] font-playwrite text-[10px] font-bold text-blackDiamond">
-                  {heldOnMonth}
-                </h6>
-
-                {/* Result  */}
-                <h6 className="absolute capitalize top-[294px] right-[157px] font-playwrite text-[10px] font-bold text-blackDiamond">
-                  {result}
-                </h6>
-
-                {/* Publication of Result  */}
-                <h6 className="absolute capitalize top-[345px] left-[175px] font-playwrite text-[8px] font-bold text-blackDiamond">
-                  {publishDate}
-                </h6>
-
-                {/* Date of issue  */}
-                <h6 className="absolute capitalize top-[362px] left-[110px] font-playwrite text-[8px] font-bold text-blackDiamond">
-                  {dateOfIssue}
-                </h6>
-                {/* Compared By  */}
-                <img
-                  src={compared}
-                  alt="Comapred signature"
-                  className="absolute bottom-[33px] left-[266px] w-[75px]"
-                />
-
-                {/* Compared By  */}
-                <img
-                  src={controller}
-                  alt="Controller Signature"
-                  className="absolute bottom-[35px] right-[85px] w-[75px]"
-                />
-              </div>
-            )}
-          </div>
         </div>
       </div>
-      <button
-            type="button"
-            className="w-full mt-5 bg-[#3AAFA9] text-white py-3 rounded-lg font-semibold font-montserrat text-lg"
-          >
-            {isLoading ? (
-              <TbFidgetSpinner className="animate-spin m-auto" />
-            ) : (
-              "Generate Certificate & Approve"
-            )}
-          </button>
     </div>
   );
 };
