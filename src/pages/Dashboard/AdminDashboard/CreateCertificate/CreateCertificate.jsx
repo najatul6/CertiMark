eb806e80329c3e212a1024c1ccdeb78e14474053
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-17 08:36:35 +0600

    update testimonial page

--- src/pages/Dashboard/AdminDashboard/CreateCertificate/CreateCertificate.jsx
+++ src/pages/Dashboard/AdminDashboard/CreateCertificate/CreateCertificate.jsx
@@ -23,10 +23,10 @@ const CreateCertificate = () => {
       <DashboardTitle title={"Create Certificate"} />
       <div className="grid grid-cols-2 place-items-center">
         <div className="w-full bg-white h-full">
-            {/* Testimonial Form  */}
-            <form></form>
-            {/* Testimonial Form  */}
-            <form></form>
+          {/* Testimonial Form  */}
+          <form></form>
+          {/* Testimonial Form  */}
+          <form></form>
         </div>
         <div className="relative">
           {/* Image  */}
@@ -50,7 +50,76 @@ const CreateCertificate = () => {
           {/* Content  */}
           <div className="absolute top-0 left-0 w-full h-full">
             {application?.certificateType === "testimonial" ? (
-              <>helo</>
+              <div className="relative w-[560px] h-[400px]">
+                {/* Student Name  */}
+                <h6 className="absolute capitalize top-[160px] right-[178px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                  Md. Najatul Islam
+                </h6>
+
+                {/* Father Name  */}
+                {/* <h6 className="absolute capitalize top-[182px] right-[165px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                  Md. Mozibur Rahman
+                </h6> */}
+
+                {/* Mother Name  */}
+                {/* <h6 className="absolute capitalize top-[202px] right-[162px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                  Mst. Khadija begum
+                </h6> */}
+
+                {/* Student Roll  */}
+                {/* <h6 className="absolute capitalize top-[250px] left-[127px] font-playwrite text-[10px] font-bold text-blackDiamond">
+                  490862
+                </h6> */}
+
+                {/* Registration Number  */}
+                {/* <h6 className="absolute top-[125px] right-[57px] font-playwrite text-[7px] font-bold text-blackDiamond">
+                1502041769
+              </h6> */}
+
+                {/* Session  */}
+                {/* <h6 className="absolute top-[145px] right-[80px] font-playwrite text-[7px] font-bold text-blackDiamond">
+                2019-20
+              </h6> */}
+
+                {/* Technology  */}
+                {/* <h6 className="absolute capitalize top-[266px] left-[180px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                  Computer Technology
+                </h6> */}
+
+                {/* Held of Month  */}
+                {/* <h6 className="absolute capitalize top-[293px] left-[65px] font-playwrite text-[10px] font-bold text-blackDiamond">
+                  December,2019 - February, 2025
+                </h6> */}
+
+                {/* Board Name  */}
+                {/* <h6 className="absolute capitalize top-[222px] right-[120px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                  Rangpur Polytechnic Institute, Rangpur
+                </h6> */}
+
+                {/* Result  */}
+                {/* <h6 className="absolute capitalize top-[294px] right-[157px] font-playwrite text-[10px] font-bold text-blackDiamond">
+                  3.72
+                </h6> */}
+
+                {/* Date of issue  */}
+                {/* <h6 className="absolute capitalize top-[362px] left-[110px] font-playwrite text-[8px] font-bold text-blackDiamond">
+                  November 11, 2025
+                </h6> */}
+
+                {/* Register Signature  */}
+                {/* <img
+                  src={compared}
+                  alt="Comapred signature"
+                  className="absolute bottom-[33px] left-[266px] w-[75px]"
+                /> */}
+
+                {/* Principal Signature  */}
+                {/* <img
+                  src={controller}
+                  alt="Controller Signature"
+                  className="absolute bottom-[35px] right-[85px] w-[75px]"
+                /> */}
+              </div>
             ) : (
               <div className="relative w-[560px] h-[400px]">
                 {/* Serial Number  */}
@@ -130,7 +199,11 @@ const CreateCertificate = () => {
                 />
 
                 {/* Compared By  */}
-                <img src={controller} alt="Controller Signature" className="absolute bottom-[35px] right-[85px] w-[75px]"/>
+                <img
+                  src={controller}
+                  alt="Controller Signature"
+                  className="absolute bottom-[35px] right-[85px] w-[75px]"
+                />
               </div>
             )}
           </div>
