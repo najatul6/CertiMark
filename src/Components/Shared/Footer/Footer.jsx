864c89cfc6db8d2a1f9c4d978cf66f87aca7694a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-18 20:18:02 +0600

    update footer

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -67,7 +67,7 @@ const Footer = () => {
 
         {/* Bottom Text */}
         <div className="text-center mt-8 text-sm">
-          <p>&copy;2024 <span className="text-teal font-bold">CertiMark</span>. All rights reserved.| Developed By - <Link target="_blank" className="text-lightTeal font-bold hover:underline" to={"https://www.najatulislam.me"}>Najatul Islam</Link></p>
+          <p>&copy; 2024 All rights reserved <span className="text-teal font-bold">CertiMark</span>.| Developed By - <Link target="_blank" className="text-lightTeal font-bold hover:underline" to={"https://www.najatulislam.me"}>Najatul Islam</Link></p>
         </div>
       </div>
     </footer>
