ce30f003b79970d52e7dd9aea8df3d27c2bf53a1
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-18 21:53:58 +0600

    change steps bg and positions

--- src/Components/HowItWork/HowItWork.jsx
+++ src/Components/HowItWork/HowItWork.jsx
@@ -18,29 +18,29 @@ const HowItWork = () => {
                     <div className="flex flex-row justify-center items-center">
                         <div className="flex flex-col items-center justify-center mr-5">
                             <div
-                                className="flex items-center justify-center h-20 w-20 rounded-full bg-indigo-500 text-white border-4 border-white text-xl font-semibold">
-                                1
+                                className="flex items-center justify-center h-20 w-20 rounded-full bg-lightTeal text-white border-4 border-white text-xl font-semibold">
+                               01
                             </div>
                             <span className="text-gray-500">STEP</span>
                         </div>
-                        <div className="bg-gray-100 p-5 pb-10 ">
-                            <h4 className="text-lg leading-6 font-semibold text-gray-900">Apply</h4>
-                            <p className="mt-2 text-base leading-6 text-gray-500">
+                        <div className="border-2 rounded-xl p-5 pb-10 ">
+                            <h4 className="text-lg leading-6 font-semibold text-lightTeal">Apply</h4>
+                            <p className="mt-2 text-base leading-6 text-teal">
                                 Apply for a certificate by filling out the form. When you will pay for this certificate the certificate will be generated.
                             </p>
                         </div>
                     </div>
                 </li>
                 <li className="text-left mb-10">
-                    <div className="flex flex-row items-start">
+                    <div className="flex flex-row justify-center items-center">
                         <div className="flex flex-col items-center justify-center mr-5">
                             <div
-                                className="flex items-center justify-center h-20 w-20 rounded-full bg-indigo-500 text-white border-4 border-white text-xl font-semibold">
-                                2
+                                className="flex items-center justify-center h-20 w-20 rounded-full bg-lightTeal text-white border-4 border-white text-xl font-semibold">
+                                02
                             </div>
                             <span className="text-gray-500">STEP</span>
                         </div>
-                        <div className="bg-gray-100 p-5 pb-10 ">
+                        <div className="border-2 rounded-xl p-5 pb-10 ">
                             <h4 className="text-lg leading-6 font-semibold text-gray-900">Check Status</h4>
                             <p className="mt-2 text-base leading-6 text-gray-500">
                                Check application status by click on the application status button. You will get the status of your application.
@@ -49,15 +49,15 @@ const HowItWork = () => {
                     </div>
                 </li>
                 <li className="text-left mb-10">
-                    <div className="flex flex-row items-start">
+                    <div className="flex flex-row justify-center items-center">
                         <div className="flex flex-col items-center justify-center mr-5">
                             <div
-                                className="flex items-center justify-center h-20 w-20 rounded-full bg-indigo-500 text-white border-4 border-white text-xl font-semibold">
+                                className="flex items-center justify-center h-20 w-20 rounded-full bg-lightTeal text-white border-4 border-white text-xl font-semibold">
                                 3
                             </div>
                             <span className="text-gray-500">STEP</span>
                         </div>
-                        <div className="bg-gray-100 p-5 pb-10 ">
+                        <div className="border-2 rounded-xl p-5 pb-10 ">
                             <h4 className="text-lg leading-6 font-semibold text-gray-900">Download</h4>
                             <p className="mt-2 text-base leading-6 text-gray-500">
                                 If your application is approved you will get the certificate. You can download the certificate from the download button.
