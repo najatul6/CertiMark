2bf9b3484b3368f96e0c88f58e1c648d11a22fc4
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-22 23:55:21 +0600

    feat: enhance testimonial certificate generation with dynamic data display and improved layout

--- src/pages/Dashboard/AdminDashboard/CreateCertificate/CreateCertificate.jsx
+++ src/pages/Dashboard/AdminDashboard/CreateCertificate/CreateCertificate.jsx
@@ -64,7 +64,7 @@ const CreateCertificate = () => {
                   if (updateData.data?.modifiedCount > 0) {
                     refetch();
                     navigate("/dashboard/pendingApplications");
-                   toast.success(
+                    toast.success(
                       "Application Approved and Updated Successfully!"
                     );
                   } else {
@@ -206,13 +206,115 @@ const CreateCertificate = () => {
           {/* Image  */}
           {application?.certificateType === "testimonial" ? (
             // Testimonial Image
-            <div className="w-[560px] h-[400px]">
-              <img
-                src={testimonial}
-                alt={`${application.name}'s certificate`}
-                className="w-[560px] h-[426px]"
-              />
-            </div>
+            <>
+              <div ref={ref} className="relative">
+                <div className="absolute w-[560px] h-[400px]">
+                  {/* Serial Number  */}
+                  <h6 className="absolute top-[108px] left-[85px] tracking-[3px] font-montserrat text-[9px] font-bold text-blackDiamond">
+                    {serialNumber}
+                  </h6>
+
+                  {/* Year  */}
+                  <h2 className="absolute top-[90px] left-[405px] font-tenor text-blackDiamond">
+                    2024
+                  </h2>
+
+                  {/* Registration Number  */}
+                  <h6 className="absolute top-[125px] right-[57px] font-playwrite text-[7px] font-bold text-blackDiamond">
+                    {application?.registrationNo}
+                  </h6>
+
+                  {/* Session  */}
+                  <h6 className="absolute top-[145px] right-[80px] font-playwrite text-[7px] font-bold text-blackDiamond">
+                    {application?.yearOfCompletion}
+                  </h6>
+
+                  {/* Student Name  */}
+                  <h6 className="absolute capitalize top-[160px] right-[178px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                    {application?.name}
+                  </h6>
+
+                  {/* Father Name  */}
+                  <h6 className="absolute capitalize top-[182px] right-[165px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                    {application?.fatherName}
+                  </h6>
+
+                  {/* Mother Name  */}
+                  <h6 className="absolute capitalize top-[202px] right-[162px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                    {application?.motherName}
+                  </h6>
+
+                  {/* College Name  */}
+                  <h6 className="absolute capitalize top-[222px] right-[120px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                    Rangpur Polytechnic Institute, Rangpur
+                  </h6>
+
+                  {/* Student Roll  */}
+                  <h6 className="absolute capitalize top-[250px] left-[127px] font-playwrite text-[10px] font-bold text-blackDiamond">
+                    {application?.studentId}
+                  </h6>
+
+                  {/* Technology  */}
+                  <h6 className="absolute capitalize top-[266px] left-[180px] font-playwrite text-[12px] font-bold text-blackDiamond">
+                    {application?.technology}
+                  </h6>
+
+                  {/* Held of Month  */}
+                  <h6 className="absolute capitalize top-[293px] left-[65px] font-playwrite text-[10px] font-bold text-blackDiamond">
+                    {heldOnMonth}
+                  </h6>
+
+                  {/* Result  */}
+                  <h6 className="absolute capitalize top-[294px] right-[157px] font-playwrite text-[10px] font-bold text-blackDiamond">
+                    {result}
+                  </h6>
+
+                  {/* Publication of Result  */}
+                  <h6 className="absolute capitalize top-[345px] left-[175px] font-playwrite text-[8px] font-bold text-blackDiamond">
+                    {publishDate}
+                  </h6>
+
+                  {/* Date of issue  */}
+                  <h6 className="absolute capitalize top-[362px] left-[110px] font-playwrite text-[8px] font-bold text-blackDiamond">
+                    {dateOfIssue}
+                  </h6>
+                  {/* Compared By  */}
+                  <img
+                    src={compared}
+                    alt="Comapred signature"
+                    className="absolute bottom-[33px] left-[266px] w-[75px]"
+                  />
+
+                  {/* Compared By  */}
+                  <img
+                    src={controller}
+                    alt="Controller Signature"
+                    className="absolute bottom-[35px] right-[85px] w-[75px]"
+                  />
+                </div>
+                <div className="w-[560px] h-[400px]">
+                  <img
+                    src={testimonial}
+                    alt={`${application.name}'s certificate`}
+                    className="w-[560px] h-[426px]"
+                  />
+                </div>
+              </div>
+              <button
+                type="button"
+                onClick={onButtonClick}
+                className="w-full mt-10 bg-[#3AAFA9] text-white py-3 rounded-lg font-semibold font-montserrat text-lg"
+              >
+                {isLoading ? (
+                  <span className="flex justify-center items-center">
+                    <div className="w-5 h-5 border-t-2 border-white rounded-full animate-spin"></div>
+                    <span className="ml-2">Processing...</span>
+                  </span>
+                ) : (
+                  "Generate Certificate & Approve"
+                )}
+              </button>
+            </>
           ) : (
             // Clearance
             <>
@@ -315,10 +417,10 @@ const CreateCertificate = () => {
                 className="w-full mt-10 bg-[#3AAFA9] text-white py-3 rounded-lg font-semibold font-montserrat text-lg"
               >
                 {isLoading ? (
-                <span className="flex justify-center items-center">
-                <div className="w-5 h-5 border-t-2 border-white rounded-full animate-spin"></div>
-                <span className="ml-2">Processing...</span>
-              </span>
+                  <span className="flex justify-center items-center">
+                    <div className="w-5 h-5 border-t-2 border-white rounded-full animate-spin"></div>
+                    <span className="ml-2">Processing...</span>
+                  </span>
                 ) : (
                   "Generate Certificate & Approve"
                 )}
