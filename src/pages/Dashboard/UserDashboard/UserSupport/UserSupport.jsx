e56aad07259683d08fced277299e50ba7d50f409
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-22 22:34:53 +0600

    feat: update UserSupport component to include support image and streamline chat layout

--- src/pages/Dashboard/UserDashboard/UserSupport/UserSupport.jsx
+++ src/pages/Dashboard/UserDashboard/UserSupport/UserSupport.jsx
@@ -1,6 +1,7 @@
 import DashboardTitle from "../../../../Components/Shared/DashboardTitle/DashboardTitle";
 import useAuth from "../../../../hooks/useAuth";
 import useMessages from "../../../../hooks/useMessages";
+import supportImg from "../../../../assets/support.png";
 
 const UserSupport = () => {
   const [message, refetch, isPending] = useMessages();
@@ -15,10 +16,7 @@ const UserSupport = () => {
             <div className="chat chat-start">
               <div className="chat-image avatar">
                 <div className="w-10 rounded-full">
-                  <img
-                    alt={user?.name}
-                    src={user.photoURL}
-                  />
+                  <img alt={user?.name} src={user.photoURL} />
                 </div>
               </div>
               <div className="chat-bubble text-white">{msg?.message}</div>
@@ -27,18 +25,10 @@ const UserSupport = () => {
             <div className="chat chat-end">
               <div className="chat-image avatar">
                 <div className="w-10 rounded-full">
-                  <img
-                    alt="Tailwind CSS chat bubble component"
-                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                  />
+                  <img alt="Support Team" src={supportImg} />
                 </div>
               </div>
-              <div className="chat-header">
-                Anakin
-                <time className="text-xs opacity-50">12:46</time>
-              </div>
               <div className="chat-bubble">I hate you!</div>
-              <div className="chat-footer opacity-50">Seen at 12:46</div>
             </div>
           </>
         ))}
