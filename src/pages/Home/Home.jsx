f6726d3599ce54a8a7029e59d798f7a007b7fd3f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-23 22:27:15 +0600

    feat: remove commented-out Testimonials component from Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,6 @@ import Hero from "../../Components/Hero/Hero";
 import HowItWork from "../../Components/HowItWork/HowItWork";
 import InformationSection from "../../Components/InformationSection/InformationSection";
 import Team from "../../Components/Team/Team";
-// import Testimonials from "../../Components/Testimonials/Testimonials";
 
 const Home = () => {
   return (
@@ -14,7 +13,6 @@ const Home = () => {
       <InformationSection />
       <Gallery />
       <ContentBlock/>
-      {/* <Testimonials /> */}
       <Team />
     </div>
   );
