8cac44773f3d69ca582b1aaa00c96996bf87dc0a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-24 02:14:14 +0600

    feat: adjust positioning and add tracking to result text element in CreateCertificate component

--- src/pages/Dashboard/AdminDashboard/CreateCertificate/CreateCertificate.jsx
+++ src/pages/Dashboard/AdminDashboard/CreateCertificate/CreateCertificate.jsx
@@ -318,7 +318,7 @@ const CreateCertificate = () => {
                   </h6>
 
                   {/* Result  */}
-                  <h6 className="absolute capitalize top-[195px] right-[180px] font-mono text-[12px] font-extrabold text-blackDiamond">
+                  <h6 className="absolute capitalize top-[195px] right-[187px] font-mono text-[12px] font-extrabold text-blackDiamond tracking-[-1px]">
                     {result}
                   </h6>
 
