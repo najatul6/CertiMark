b19c235438bcb138503b57ffaebbc08bb20ff609
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-23 21:27:25 +0600

    fix: improve accessibility by enhancing contrast in input fields across components

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,13 +1,9 @@
-import image1 from '../../assets/gallery/1.HEIC'
-import image2 from '../../assets/gallery/2.HEIC'
-import image3 from '../../assets/gallery/3.HEIC'
-import image4 from '../../assets/gallery/4.HEIC'
-import image5 from '../../assets/gallery/5.HEIC'
-import image6 from '../../assets/gallery/6.HEIC'
-import image7 from '../../assets/gallery/7.HEIC'
-import image8 from '../../assets/gallery/8.HEIC'
-import image9 from '../../assets/gallery/9.HEIC'
-import image10 from '../../assets/gallery/10.HEIC'
+import image1 from '../../assets/gallery/1.jpg'
+import image2 from '../../assets/gallery/2.jpg'
+import image3 from '../../assets/gallery/3.jpg'
+import image4 from '../../assets/gallery/4.jpg'
+import image5 from '../../assets/gallery/5.jpg'
+import image6 from '../../assets/gallery/6.jpg'
 
 const images = [
   {
@@ -34,22 +30,7 @@ const images = [
     src: `${image6}`,
     alt: "Photo by Najatul islam",
   },
-  {
-    src: `${image7}`,
-    alt: "Photo by Najatul islam",
-  },
-  {
-    src: `${image8}`,
-    alt: "Photo by Najatul islam",
-  },
-  {
-    src: `${image9}`,
-    alt: "Photo by Najatul islam",
-  },
-  {
-    src: `${image10}`,
-    alt: "Photo by Najatul islam",
-  },
+  
   
 ];
 
