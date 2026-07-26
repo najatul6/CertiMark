c1c86bb2abf5ed64fdc3307ddceeceb453350026
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-07-26 17:47:58 +0600

    fix: increase z-index for modal in Gallery component

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -70,7 +70,7 @@ const Gallery = () => {
       </div>
       {selectedImage && (
         <div
-          className="fixed inset-0 z-50 flex items-center justify-center bg-black/80 p-4"
+          className="fixed inset-0 z-10000 flex items-center justify-center bg-black/80 p-4"
           onClick={() => setSelectedImage(null)}
         >
           <div
